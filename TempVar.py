414b73227274a5ab80cdbb2acb578557118dd0d1
barunps3 <47608189+barunps3@users.noreply.github.com>
2019-03-20 15:59:52 +0100

    Update TempVar.py

--- TempVar.py
+++ TempVar.py
@@ -1,3 +1,75 @@
+def getTliqCoordEx(set_name_assembly, T_liq, Results_Dict_mech, DOF):
+
+
+    CoordKey = 'COORD' + str(DOF)
+
+    # Dataframe containing the node set time series is collected
+    ResDF_Temp = getArrayfromSetDict(Results_Dict_mech, set_name_assembly, 'NT11', 'N')
+    ResDF_Coord = getArrayfromSetDict(Results_Dict_mech, set_name_assembly, CoordKey, 'N')
+    
+    # Collect the column names (Node names)
+    KeyList = list(ResDF_Temp.keys())
+    
+    MaxValDict = {}
+    
+    # iterate via each key in KeyList and
+    # Take out the maximum value in the time for EACH node and 
+    # Add to MaxValDict with key name as the Node names
+    for Key in KeyList:
+        
+        MaxValDict[Key] = ResDF_Temp[Key].max()
+    
+    # Convert the dictionary with key and max. value into a list of tuple pairs and 
+    # sorts the tuple pairs based on max.value in ascending order
+    sorted_MaxVals = sorted(MaxValDict.items(), key=operator.itemgetter(1))
+
+    
+    try:
+        # assign index key to each tuple pair in the sorted list and
+        # iterate through each tuple(index, (node name, max. val)) in list
+        for Pos, NT_Tuple in enumerate(sorted_MaxVals):
+            # if max value is greater than liquid temp then
+            if NT_Tuple[1] >= T_liq:
+                # collect the index values (Pos) of these Tuple pairs
+                Pos_aboveLiq = Pos
+                # subtract the index by 1 and collect the subtracted value as Position of 
+                # tuple as below liquid temp.
+                Pos_belowLiq = Pos - 1
+                break
+        
+        # collect the last assigned node name of the max. val greater than T-liq 
+        Node_aboveLiq = sorted_MaxVals[Pos_aboveLiq][0]
+        # collect the last assigned node name for Pos_belowLiq
+        Node_belowLiq = sorted_MaxVals[Pos_belowLiq][0]
+        
+        # collect the corresponding temperatures
+        NT_aboveLiq = sorted_MaxVals[Pos_aboveLiq][1]
+        NT_belowLiq = sorted_MaxVals[Pos_belowLiq][1]
+
+        # Access the columns with the node names and 
+        # collect the last row COORD value for the corresponding node name
+        Coord_aboveLiq = ResDF_Coord[Node_aboveLiq].iloc[-1]
+        Coord_belowLiq = ResDF_Coord[Node_belowLiq].iloc[-1]
+
+        # pair the two temperatures and COORDs
+        xp = [NT_belowLiq, NT_aboveLiq]
+        fp = [Coord_belowLiq, Coord_aboveLiq]
+
+        # Calculate the interpolated fp value between the two temperatures at liquid temperature
+        Coordinate_Tliq = np.interp(T_liq, xp, fp)        
+        # Gives the Coordinate
+        Weld_Rad_Coord = Coordinate_Tliq
+
+    except:
+
+        Weld_Rad_Coord = 0.0
+
+    # print(Weld_Rad)
+
+    return Weld_Rad_Coord
+
+
+
 def getTliqCoordTS(set_name_assembly, T_Liq, Results_Dict_mech, DOF):
 
 
@@ -50,42 +122,45 @@ def getTliqCoordTS(set_name_assembly, T_Liq, Results_Dict_mech, DOF):
         
         # loop over [(0, node1), (1, node2), ..] 
         for IdentIndex, NodeIdent in enumerate(CoordSorted_NodeList):
-            # If any value is less than or equal to T_Liq
+            # If temperature value at the give node name and Index is less than or equal to T_Liq
             if TempPerFrame_DF[NodeIdent].iloc[0] <= T_Liq:
                 #  and if :
                 if IdentIndex == 0:
-
+                    # assign the Coord variables as = 0.0
                     Coord_belowLiq = 0.0
                     Coord_aboveLiq = 0.0
-                    
+                    # collect the temperatures for given node name
                     Temperature_belowLiq = TempPerFrame_DF[NodeIdent].iloc[0]
                     Temperature_aboveLiq = TempPerFrame_DF[NodeIdent].iloc[0]
                     TimeActual = TimePerFrame_DF[NodeIdent].iloc[0]
 
                 else:
-
+                    # assign the node name 
                     Node_belowLiq = NodeIdent
+                    # collect the node name at index value 'IdentIndex-1' from list CoordSorted_NodeList
                     Node_aboveLiq = CoordSorted_NodeList[IdentIndex-1]
-
+                    # collect the COORDs for both node names
                     Coord_belowLiq = CoordPerFrame_DF[Node_belowLiq].iloc[0]
                     Coord_aboveLiq = CoordPerFrame_DF[Node_aboveLiq].iloc[0]
-
+                    # collect the respective temperatures for both node names 
                     Temperature_belowLiq = TempPerFrame_DF[Node_belowLiq].iloc[0]
                     Temperature_aboveLiq = TempPerFrame_DF[Node_aboveLiq].iloc[0]
-
+                    # assign the current time value for node name - NodeIdent 
                     TimeActual = TimePerFrame_DF[NodeIdent].iloc[0]
-
-    #            UpperNodeIdentList.append(copy.copy(Node_belowLiq))
+                    
+                # Append the Coord_belowLiq to the list CoordList_belowLiq
+                # UpperNodeIdentList.append(copy.copy(Node_belowLiq))
                 CoordList_belowLiq.append(copy.copy(Coord_belowLiq))
+                # Append the CoordList_aboveLiq to the list Coord_aboveLiq
                 CoordList_aboveLiq.append(copy.copy(Coord_aboveLiq))
-
+                # Append the temperatures to their respective temperature lists
                 TemperatureList_belowLiq.append(copy.copy(Temperature_belowLiq))
                 TemperatureList_aboveLiq.append(copy.copy(Temperature_aboveLiq))
-
+                # Append the time to Time List
                 TimeList.append(copy.copy(TimeActual))
-
+                # break the for loop completely
                 break
-
+        
         xp = [Temperature_belowLiq, Temperature_aboveLiq]
         fp = [Coord_belowLiq, Coord_aboveLiq]
 
